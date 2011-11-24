9c7a405dfe0037df0c652710525ecba9dff3929e
piton <piton@71442452-7e2d-4386-8493-24728dadb4d7>
2011-11-24 08:27:22 +0000

    added gui to ESL
    git-svn-id: https://erg.biophys.msu.ru/svn/avr@30 71442452-7e2d-4386-8493-24728dadb4d7

--- ESL.py
+++ ESL.py
@@ -27,3 +27,45 @@ class ESL:
   def set_params(self,t,n,t1,w,amp):
     self.devh.controlMsg(usb.TYPE_VENDOR | usb.RECIP_DEVICE | usb.ENDPOINT_OUT, 2,\
       struct.pack("<hhhhh", ms2tick*t, n , ms2tick*t1, ms2tick*w, amp))
+if __name__=="__main__":
+  from Tkinter import *
+
+  root=Tk()
+  root.title("ElectroStimuLator")
+  root.resizable(0,0)
+
+  frInp=Frame(root)
+  frInp.pack(side=TOP)
+  frBut=Frame(root)
+  frBut.pack(side=TOP)
+
+  frLabels=Frame(frInp)
+  frLabels.pack(side=LEFT)
+  frInputs=Frame(frInp)
+  frInputs.pack(side=LEFT)
+ 
+  Label(frLabels,text="T, ms").pack(side=TOP,anchor='nw')
+  enT=Entry(frInputs)
+  enT.pack(side=TOP,anchor='nw')
+ 
+  Label(frLabels,text="N").pack(side=TOP,anchor='nw')
+  enN=Entry(frInputs)
+  enN.pack(side=TOP,anchor='nw')
+
+  Label(frLabels,text="T1, ms").pack(side=TOP,anchor='nw')
+  enT1=Entry(frInputs)
+  enT1.pack(side=TOP,anchor='nw')
+
+  Label(frLabels,text="w, ms").pack(side=TOP,anchor='nw')
+  enW=Entry(frInputs)
+  enW.pack(side=TOP,anchor='nw')
+
+  Label(frLabels,text="A, mV").pack(side=TOP,anchor='nw')
+  enA=Entry(frInputs)
+  enA.pack(side=TOP,anchor='nw')
+  
+  Button(frBut, text="Apply",command=lambda x=1: 1).pack(side=LEFT)
+  Button(frBut, text="Start").pack(side=LEFT)
+  Button(frBut, text="Single").pack(side=LEFT)
+  Button(frBut, text="Stop").pack(side=LEFT)
+  root.mainloop()
