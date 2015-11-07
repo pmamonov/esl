0e4e13e7d4b846bfee6ee77f8d75e08ae7b42731
Peter Mamonov <pmamonov@gmail.com>
2015-11-07 19:37:54 +0300

    add esl_single.py script
    esl_single.py is a sample script which drives ESL output high
    for arbitrary amount of time.

---
+++ esl_single.py
@@ -0,0 +1,32 @@
+import sys
+from time import sleep
+from ESL import ESL, ms2tick
+
+if len(sys.argv) < 2:
+	print """
+USAGE: %s NUMBER
+	NUMBER - pulse duration in seconds, e.g. 4.2.
+""" % sys.argv[0]
+	exit(1)
+
+# parse user-supplied number of seconds
+delay = float(sys.argv[1])
+# find the device
+esl = ESL()
+# setup stimulation parameters
+t = 100
+p = {
+	't':	t * ms2tick,
+	'n':	1,
+	't1':	t * ms2tick,
+	'w':	t * ms2tick,
+	'a':	1,
+}
+# apply stimulation parameters
+esl.set_params(**p)
+# start stimulation
+esl.start()
+# wait for user-supplied number of seconds
+sleep(delay)
+# stop stimulation
+esl.stop()
