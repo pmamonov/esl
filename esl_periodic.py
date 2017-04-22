a4e482d0089d4c3a822c44de0b52dd9b30ac0361
Peter Mamonov <pmamonov@gmail.com>
2017-04-22 16:26:10 +0300

    add esl_periodic.py

---
+++ esl_periodic.py
@@ -0,0 +1,27 @@
+import sys
+from time import sleep, time
+from ESL import ESL, ms2tick
+
+period = 1. # s
+length = 10 # ms
+
+p = {
+	't':	length * ms2tick,
+	'n':	1,
+	't1':	length * ms2tick,
+	'w':	length * ms2tick,
+	'a':	1,
+}
+
+# find the device
+esl = ESL()
+# apply stimulation parameters
+esl.set_params(**p)
+
+t = time() + period
+while True:
+	print t;
+	esl.single()
+	while (time() < t):
+		sleep(.01)
+	t += period
