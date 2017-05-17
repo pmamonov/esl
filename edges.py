d99f7403118fa3663c437e0cff793270d2331aaa
Peter Mamonov <pmamonov@gmail.com>
2017-05-17 22:54:39 +0300

    add python module for gpio-poll logs processing

---
+++ edges.py
@@ -0,0 +1,30 @@
+from numpy import loadtxt
+
+def pulse_stat(fn):
+	tv = loadtxt(fn)
+	
+	for i in xrange(tv.shape[0]):
+		if tv[i,1] == 0:
+			tv = tv[i:]
+			break
+
+	posed = tv[1:,1] - tv[:-1,1] == 1
+	neged = tv[1:,1] - tv[:-1,1] == -1
+	tposed = tv[1:,0][posed]
+	tneged = tv[1:,0][neged]
+	w = tneged - tposed
+	dtposed = tposed[1:] - tposed[:-1]
+
+	print "pulse count: %d" % tposed.shape[0]
+	m = dtposed.mean()
+	print "period: %.6f %.6f +%.6f %.6f" % (m,
+				    dtposed.min() - m,
+				    dtposed.max() - m,
+				    dtposed.std())
+	m = w.mean()
+	print "width: %.6f %.6f +%6f %.6f" % (m,
+					w.min() - m,
+					w.max() - m,
+					w.std())
+	return dtposed, w
+
